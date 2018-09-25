1fd7becf773f29cafa4e630203e6af39116a1b12
Chris Klimowski <csklimowski@email.arizona.edu>
2018-09-25 16:55:25 -0600

    moved file retrieval to component constructor

--- dashboard/src/js/main.jsx
+++ dashboard/src/js/main.jsx
@@ -1,12 +1,5 @@
 import { FileTable } from './fileTable';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
 
-axios.get('/api/files/top')
-.then(function(response) {
-    ReactDOM.render(<FileTable files={response.data}/>, document.getElementById('fileTable'))
-})
-.catch(function(error) {
-    console.log(error);
-});
+ReactDOM.render(<FileTable />, document.getElementById('fileTable'))
