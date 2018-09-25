094c44d608b4c7d288510d5f738d7f96783bc37a
Chris Klimowski <csklimowski@email.arizona.edu>
2018-09-25 16:52:41 -0600

    moved template to top-level folder

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -55,8 +55,10 @@ ROOT_URLCONF = 'urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
