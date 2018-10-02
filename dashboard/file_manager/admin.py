1f2c9b4d5127671ed5da5f77b55b0b66b6618ed9
Chris Klimowski <csklimowski@email.arizona.edu>
2018-10-02 17:02:49 -0600

    better admin for models

--- dashboard/file_manager/admin.py
+++ dashboard/file_manager/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 
-from .models import File, Folder
+from .models import File, Folder, FileType, UpdateLog
 
-admin.site.register(File)
-admin.site.register(Folder)
+
+class FileAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'size', 'date_created', 'file_type', 'path'
+    )
+    search_fields = (
+        'id', 'name', 'path'
+    )
+
+
+class FolderAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'total_size', 'path'
+    )
+    search_fields = (
+        'id', 'name', 'path'
+    )
+
+admin.site.register(File, FileAdmin)
+admin.site.register(Folder, FolderAdmin)
+admin.site.register(FileType)
+admin.site.register(UpdateLog)
