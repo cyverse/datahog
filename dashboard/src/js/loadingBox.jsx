4587f59d67abe945ffbf3f388de1b82f59aae993
Chris Klimowski <csklimowski@email.arizona.edu>
2018-10-11 17:03:34 -0600

    new "loading box" to hide contents while waiting for requests to finish

---
+++ dashboard/src/js/loadingBox.jsx
@@ -0,0 +1,93 @@
+import axios from 'axios';
+import React from 'react';
+
+export class LoadingBox extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+        axios.defaults.xsrfCookieName = "csrftoken";
+
+        this.getStatus = this.getStatus.bind(this);
+        this.getStatus();
+
+        this.state = {
+            interval: setInterval(this.getStatus, 10000),
+            updateInProgress: false,
+            error: false,
+            loading: true
+        };
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.state.interval);
+    }
+
+    getStatus() {
+        axios.get('/api/summaries/totals')
+        .then(function(response) {
+            if (response.data.in_progress) {
+                this.setState({
+                    updateInProgress: true,
+                    error: false,
+                    loading: false,
+                    interval: this.state.interval
+                });
+            } else {
+                this.setState({
+                    updateInProgress: false,
+                    error: false,
+                    loading: false,
+                    interval: this.state.interval
+                });
+            }
+        }.bind(this))
+        .catch(function(error) {
+            this.setState({
+                updateInProgress: false,
+                error: true,
+                loading: false,
+                interval: this.state.interval
+            });
+        }.bind(this));
+    }
+
+    render() {
+        if (this.state.updateInProgress) return (
+            <div className="empty">
+                <div className="empty-icon">
+                    <i className="icon icon-people"></i>
+                </div>
+                <p className="empty-title h5">You have no new messages</p>
+                <p className="empty-subtitle">Click the button to start a conversation.</p>
+                <div className="empty-action">
+                    <button className="btn btn-primary">Send a message</button>
+                </div>
+            </div>
+        );
+
+        if (this.state.error || this.props.childError) return (
+            <div className="empty">
+                <div className="empty-icon">
+                    <i className="icon icon-people"></i>
+                </div>
+                <p className="empty-title h5">You have no new messages</p>
+                <p className="empty-subtitle">Click the button to start a conversation.</p>
+                <div className="empty-action">
+                    <button className="btn btn-primary">Send a message</button>
+                </div>
+            </div>
+        );
+
+        if (this.state.loading || this.props.childLoading) return (
+            <div className="loading loading-lg"></div>
+        );
+
+        return (
+            <React.Fragment>
+                {this.props.children}
+            </React.Fragment>
+        );
+    }
+}
