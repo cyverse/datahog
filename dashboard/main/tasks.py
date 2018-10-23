cbd0b5b8d3bdccd69c1f7baf625c30e17648bc18
Chris Klimowski <csklimowski@email.arizona.edu>
2018-10-23 16:57:36 -0600

    file types are created as the import happens

--- dashboard/main/tasks.py
+++ dashboard/main/tasks.py
@@ -1,9 +1,10 @@
-from celery import shared_task
-from django.db import transaction
 import time
 import re
 import json
 import datetime
+
+from celery import shared_task
+from django.db import transaction
 
 from .models import *
 
@@ -23,45 +24,46 @@ def update_database_from_file(update_id):
             file_count = 0
             total_size = 0
 
-            FileType.objects.update(total_size=0)
+            FileType.objects.all().delete()
             File.objects.all().delete()
             Folder.objects.all().delete()
 
             for file in files:
-                # parse file information
-                last_slash = file['path'].rfind('/')
-                if last_slash <= 0:
-                    file_name = file['path']
-                else:
-                    file_name = file['path'][last_slash+1:]
 
-                file_size = file['size']
+                # update stats
+                file_count += 1
+                total_size += file['size']
+                if (file_count % 100 == 0):
+                    print('{} files created'.format(file_count))
+
+                # parse file information
+                file_path = '{}/{}'.format(file['collection'], file['name'])
                 file_date = datetime.datetime.strptime(file['created'], r'%Y-%m-%d %H:%M:%S')
 
                 # create file object
                 file_obj = File.objects.create(
-                    name=file_name,
-                    path=file['path'],
+                    name=file['name'],
+                    path=file_path,
                     size=file['size'],
-                    date_created=file_date
+                    date_created=file_date,
+                    checksum=file['checksum']
                 )
-                print('creating file {}...'.format(file_count+1))
                 
-                # update parent folders
-                parent_path = file_obj.path[:last_slash]
+                # create/update parent folders
+                parent_path = file['collection']
                 child_obj = file_obj
 
                 while len(parent_path) > 0:
                     parent_slash = parent_path.rfind('/')
                     try:
                         parent_obj = Folder.objects.get(path=parent_path)
-                        parent_obj.total_size += file_size
+                        parent_obj.total_size += file['size']
                         parent_obj.save()
                     except Folder.DoesNotExist:
                         parent_obj = Folder.objects.create(
                             path=parent_path,
                             name=parent_path[parent_slash+1:],
-                            total_size=file_size
+                            total_size=file['size']
                         )
                         print('creating folder {}...'.format(folder_count+1))
                         folder_count += 1
@@ -71,22 +73,24 @@ def update_database_from_file(update_id):
                     child_obj = parent_obj
                     parent_path = parent_path[:parent_slash]
 
-                last_dot = file_name.rfind('.')
-                if last_dot == -1:
-                    file_extension = ''
-                else:
-                    file_extension = file_name[last_dot+1:]
-                
-                for ft in FileType.objects.all():
-                    if re.fullmatch(ft.extension_pattern, file_extension) is not None:
-                        file_obj.file_type = ft
-                        ft.total_size += file_size
-                        ft.save()
-                        file_obj.save()
-                        break
+                # fill file type
+                last_dot = file['name'].rfind('.')
+                if last_dot != -1:
+                    file_extension = file['name'][last_dot+1:]
+                    try:
+                        ft = FileType.objects.get(extension_pattern=file_extension)
+                    except FileType.DoesNotExist:
+                        ft = FileType.objects.create(
+                            name=file_extension,
+                            extension_pattern=file_extension,
+                            total_size=0
+                        )
+                    ft.total_size += file['size']
+                    ft.save()
+                    file_obj.file_type = ft
+                    file_obj.save()
 
-                file_count += 1
-                total_size += file_size        
+                # check if duplicate
 
             update_log.folder_count = folder_count
             update_log.file_count = file_count
