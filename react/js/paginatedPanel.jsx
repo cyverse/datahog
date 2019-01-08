746d096e6a919993d46247490a2ec4b62941335a
Chris Klimowski <csklimowski@email.arizona.edu>
2019-01-08 15:03:25 -0700

    separated pagination buttons component from paginated panel

--- react/js/paginatedPanel.jsx
+++ react/js/paginatedPanel.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios';
+import { Paginator } from './paginator';
 
 export class PaginatedPanel extends React.Component {
     constructor(props) {
@@ -8,29 +8,25 @@ export class PaginatedPanel extends React.Component {
         this.state = {
             files: [],
             loading: true,
-            error: false,
-            prev: null,
-            next: null,
-            page: 0
+            error: false
         };
 
         this.onLoad = this.onLoad.bind(this);
         this.onError = this.onError.bind(this);
-        this.prevPage = this.prevPage.bind(this);
-        this.nextPage = this.nextPage.bind(this);
-
-        axios.get(this.props.get + '?limit=10')
-        .then(this.onLoad)
-        .catch(this.onError);
+        this.onClick = this.onClick.bind(this);
     }
 
-    onLoad(response) {
+    onClick() {
         this.setState({
-            files: response.data.results,
+            loading: true
+        });
+    }
+
+    onLoad(files) {
+        this.setState({
+            files: files,
             loading: false,
-            error: false,
-            prev: response.data.previous,
-            next: response.data.next
+            error: false
         });
     }
 
@@ -38,31 +34,7 @@ export class PaginatedPanel extends React.Component {
         this.setState({
             files: [],
             loading: false,
-            error: true,
-            prev: null,
-            next: null
-        });
-    }
-
-    nextPage() {
-        axios.get(this.props.get + '?limit=10&offset=' + (this.state.page+1)*10)
-        .then(this.onLoad)
-        .catch(this.onError);
-
-        this.setState({
-            loading: true,
-            page: this.state.page + 1
-        });
-    }
-
-    prevPage() {
-        axios.get(this.props.get + '?limit=10&offset=' + (this.state.page-1)*10)
-        .then(this.onLoad)
-        .catch(this.onError);
-        
-        this.setState({
-            loading: true,
-            page: this.state.page - 1
+            error: true
         });
     }
 
@@ -75,31 +47,25 @@ export class PaginatedPanel extends React.Component {
         } else {
             panelBody = <this.props.component files={this.state.files}/>
         }
-        
+
         return (
             <div className={this.props.scroll ? "panel fixed-height" : "card fixed-height"}>
                 <div className={this.props.scroll ? "panel-header" : "card-header"}>
-                    <div className={this.props.scroll ? "panel-title h5" : "card-title h5"}>{this.props.title}</div>
+                    <div className={this.props.scroll ? "panel-title h5" : "card-title h5"}>
+                        {this.props.title}
+                    </div>
                 </div>
                 <div className={this.props.scroll ? "panel-body" : "card-body"}>
                     {panelBody}
                 </div>
                 <div className={this.props.scroll ? "panel-footer" : "card-footer"}>
-                    <div className="float-left text-gray">
-                        {!this.state.loading && (this.state.page*10 + 1) + '-' + (this.state.page*10 + this.state.files.length)}
-                    </div>
-                    <div className="btn-group float-right">
-                        <button className="btn btn-action btn-sm" 
-                            onClick={this.prevPage}
-                            disabled={this.state.loading || !this.state.prev}>
-                                <i className="fa fa-caret-left"></i>
-                            </button>
-                        <button className="btn btn-action btn-sm"
-                            onClick={this.nextPage}
-                            disabled={this.state.loading || !this.state.next}>
-                                <i className="fa fa-caret-right"></i>
-                        </button>
-                    </div>
+                    <Paginator
+                        get={this.props.get}
+                        limit={10}
+                        onLoad={this.onLoad}
+                        onError={this.onError}
+                        onClick={this.onClick}
+                    />
                 </div>
             </div>
         );
