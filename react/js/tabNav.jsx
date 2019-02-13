08f66ec184be4dc4e04c6a56fce622d0449feecd
Chris Klimowski <csklimowski@email.arizona.edu>
2019-02-13 15:44:41 -0700

    search params are saved when switching tabs

--- react/js/tabNav.jsx
+++ react/js/tabNav.jsx
@@ -2,29 +2,39 @@ import React from 'react';
 import { HashRouter, Route, Switch, NavLink } from 'react-router-dom';
 import { UpdateBox } from './updateBox';
 
-export function TabNav(props) {
-    return (
-        <HashRouter>
-            <UpdateBox>
-                <React.Fragment>
-                    <ul className='tab tab-block'>
-                        {props.tabs.map((tab, index) => {
-                            return (
-                                <li key={index} className='tab-item c-hand'>
-                                    <NavLink to={tab.path}>
-                                        {tab.name}
-                                    </NavLink>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                    <Switch>
-                        {props.tabs.map((tab, index) => {
-                            return <Route key={index}  path={tab.path} component={tab.component} />;
-                        })}
-                    </Switch>
-                </React.Fragment>
-            </UpdateBox>
-        </HashRouter>
-    );
+export const SearchContext = React.createContext({});
+
+export class TabNav extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <SearchContext.Provider value={{}}>
+                <HashRouter>
+                    <UpdateBox>
+                        <React.Fragment>
+                            <ul className='tab tab-block'>
+                                {this.props.tabs.map((tab, index) => {
+                                    return (
+                                        <li key={index} className='tab-item c-hand'>
+                                            <NavLink to={tab.path}>
+                                                {tab.name}
+                                            </NavLink>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                            <Switch>
+                                {this.props.tabs.map((tab, index) => {
+                                    return <Route key={index}  path={tab.path} component={tab.component} />;
+                                })}
+                            </Switch>
+                        </React.Fragment>
+                    </UpdateBox>
+                </HashRouter>
+            </SearchContext.Provider>
+        );
+    }
 }
