556af5d7143b1e4c80ddd254a6897454f07b7894
Chris Klimowski <csklimowski@email.arizona.edu>
2019-03-17 14:04:00 -0700

    tracking of imported directories

--- django/apps/importer/models.py
+++ django/apps/importer/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class ImportAttempt(models.Model):
-    timestamp = models.DateTimeField(auto_now_add=True)
+    date_imported = models.DateTimeField(auto_now_add=True)
     in_progress = models.BooleanField(default=False)
     current_step = models.IntegerField(default=0)
     failed = models.BooleanField(default=False)
@@ -10,4 +10,14 @@ class ImportAttempt(models.Model):
     irods_host = models.CharField(max_length=64, blank=True, default='data.cyverse.org')
     irods_port = models.CharField(max_length=64, blank=True, default='1247')
     irods_zone = models.CharField(max_length=64, blank=True, default='iplant')
-    top_folder = models.CharField(max_length=512, blank=True)
+    root_path = models.CharField(max_length=512, blank=True)
+
+
+class ImportedDirectory(models.Model):
+    import_attempt = models.ForeignKey(ImportAttempt, on_delete=models.CASCADE)
+    name = models.CharField(max_length=132)
+    # .../csklimowski/home (iRODS collection)
+    # .../shared/SOBS (iRODS collection)
+    # 
+    db_name = models.CharField(max_length=128)
+
