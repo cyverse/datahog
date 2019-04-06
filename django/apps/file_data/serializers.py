17f88a9d2dc06e8fde616e312e835085e84181b3
Chris Klimowski <csklimowski@email.arizona.edu>
2019-04-05 17:01:44 -0700

    dupe group serialization fix

--- django/apps/file_data/serializers.py
+++ django/apps/file_data/serializers.py
@@ -2,11 +2,20 @@ from rest_framework import serializers
 from .models import *
 
 
+class ImportedDirectorySerializer(serializers.ModelSerializer):
+    date_scanned = serializers.DateTimeField(format=r'%Y-%m-%d %H:%M')
+    class Meta:
+        model = ImportedDirectory
+        fields = ('id', 'date_scanned', 'folder_count', 'file_count', 'root_path', 
+            'duplicate_count', 'total_size', 'size_timeline_data', 'type_chart_data', 'directory_type')
+
+
 class FileSerializer(serializers.ModelSerializer):
     date_created = serializers.DateTimeField(format=r'%Y-%m-%d')
+    #directory_type = serializers.CharField(source='directory.directory_type', read_only=True)
     class Meta:
         model = File
-        fields = ('id', 'name', 'size', 'path', 'date_created', 'is_folder')
+        fields = ('id', 'name', 'size', 'path', 'date_created', 'is_folder',)
 
 
 class FolderSerializer(serializers.ModelSerializer):
@@ -16,21 +25,14 @@ class FolderSerializer(serializers.ModelSerializer):
 
 
 class DupeGroupSerializer(serializers.ModelSerializer):
+    files = FileSerializer(many=True)
     class Meta:
         depth = 1
         model = DupeGroup
-        fields = ('checksum', 'files', 'file_size', 'file_count')
+        fields = ('checksum', 'files', 'file_size', 'file_count', 'directories',)
 
 
 class FileTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = FileType
         fields = ('id', 'extension', 'total_size')
-
-
-class ImportedDirectorySerializer(serializers.ModelSerializer):
-    date_scanned = serializers.DateTimeField(format=r'%Y-%m-%d %H:%M')
-    class Meta:
-        model = ImportedDirectory
-        fields = ('id', 'date_scanned', 'folder_count', 'file_count', 'root_path', 
-            'duplicate_count', 'total_size', 'size_timeline_data', 'type_chart_data', 'directory_type')
