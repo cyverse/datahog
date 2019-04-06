4c1172ba29e625024d3e3152ed8b159b0cfb04b6
Chris Klimowski <csklimowski@email.arizona.edu>
2019-04-05 17:02:59 -0700

    new loading box

---
+++ react/js/browse/loadingBox.jsx
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import React from 'react';
+
+export class LoadingBox extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            error: false,
+            loading: true
+        };
+    }
+
+    componentDidMount() {
+        axios.get(this.props.get)
+        .then(function(response) {
+            this.setState({
+                loading: false,
+                error: false
+            });
+            this.props.callback(response);
+        }.bind(this))
+        .catch(function(error) {
+            this.setState({
+                loading: false,
+                error: true
+            });
+        }.bind(this));
+    }
+
+    render() {
+        if (this.state.error) return (
+            <div className="empty">
+                <p className="empty-title h5">Unable to retrieve files</p>
+                <p className="empty-subtitle">An error occurred, please try again later</p>
+            </div>
+        );
+
+        if (this.state.loading) return (
+            <div className="loading loading-lg"></div>
+        );  
+
+        return (
+            <React.Fragment>
+                {this.props.children}
+            </React.Fragment>
+        );
+    }
+}
