a4dfb85306e11968df625373d00e0dd3f6a0936e
Chris Klimowski <csklimowski@email.arizona.edu>
2019-04-08 16:36:24 -0700

    s3 fields

--- django/apps/importer/models.py
+++ django/apps/importer/models.py
@@ -5,6 +5,7 @@ class ImportAttempt(models.Model):
     IMPORT_METHOD_CHOICES = (
         ('iRODS', 'iRODS'),
         ('CyVerse', 'CyVerse'),
+        ('S3', 'S3'),
         ('File', 'File')
     )
 
@@ -13,8 +14,9 @@ class ImportAttempt(models.Model):
     current_step = models.IntegerField(default=0)
     failed = models.BooleanField(default=False)
     username = models.CharField(max_length=64, blank=True)
+    root_path = models.CharField(max_length=512, blank=True)
+    import_method = models.CharField(max_length=1, blank=True, choices=IMPORT_METHOD_CHOICES, default='File')
+    irods_zone = models.CharField(max_length=64, blank=True, default='iplant')
     irods_host = models.CharField(max_length=64, blank=True, default='data.cyverse.org')
     irods_port = models.CharField(max_length=64, blank=True, default='1247')
-    root_path = models.CharField(max_length=512, blank=True)
-    irods_zone = models.CharField(max_length=64, blank=True, default='iplant')
-    import_method = models.CharField(max_length=1, blank=True, choices=IMPORT_METHOD_CHOICES, default='File')
+    s3_bucket = models.CharField(max_length=64, blank=True)
