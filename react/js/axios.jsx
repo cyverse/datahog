099529cc26bcbc89812377233b6a5cb5555a5eea
Chris Klimowski <csklimowski@email.arizona.edu>
2019-04-16 16:22:00 -0700

    axios fixes

--- react/js/axios.jsx
+++ react/js/axios.jsx
@@ -3,6 +3,4 @@ import axios from 'axios';
 axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
 axios.defaults.xsrfCookieName = "csrftoken";
 
-const cancel = axios.CancelToken.source();
-
-export { axios, cancel };
+export default axios;
