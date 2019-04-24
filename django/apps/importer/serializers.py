9ec988f82f0a94f17173eae5475b2afcf3943af9
Chris Klimowski <csklimowski@email.arizona.edu>
2019-04-24 16:39:34 -0700

    removed import method from attempt

--- django/apps/importer/serializers.py
+++ django/apps/importer/serializers.py
@@ -7,4 +7,6 @@ class ImportAttemptSerializer(serializers.ModelSerializer):
     class Meta:
         model = ImportAttempt
         fields = ('id', 'date_imported', 'in_progress', 'current_step', 'failed', 
-            'username', 'irods_host', 'irods_port', 'irods_zone', 'root_path', 'source_name')
+            'irods_user', 'irods_host', 'irods_port', 'irods_zone', 'irods_root', 'irods_name',
+            'cyverse_user', 'cyverse_root', 'cyverse_name', 'file_name', 
+            's3_key', 's3_bucket', 's3_root', 's3_name')
